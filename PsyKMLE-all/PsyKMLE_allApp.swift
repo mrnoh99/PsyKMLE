cdf39b9a8a05f1e9effd8f7413f78e7b507a9163
jai sung NOH <jsnoh@ajou.ac.kr>
2025-08-02 15:20:17 +0900

    Initial Commit

---
+++ PsyKMLE-all/PsyKMLE_allApp.swift
@@ -0,0 +1,17 @@
+//
+//  PsyKMLE_allApp.swift
+//  PsyKMLE-all
+//
+//  Created by Jai Sung NOH on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PsyKMLE_allApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
